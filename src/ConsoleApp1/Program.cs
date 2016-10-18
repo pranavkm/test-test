259ca2b87b758bc28ecc60b2f10772aaa8f93775
Pranav Krishnamoorthy <prkrishn@microsoft.com>
2016-10-18 15:54:51 -0700

    Initial commit - nunit

---
+++ src/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+using NUnit.Framework;
+
+namespace ConsoleApp1
+{
+    [TestFixture]
+    public class Program
+    {
+        [Test]
+        public void SimpleTest()
+        {
+            var value = "Hello";
+
+            var value2 = "Hello";
+
+            Assert.AreEqual(value, value2);
+        }
+
+        [Test]
+        public void SimpleTest2()
+        {
+            var value = "Hello";
+
+            var value2 = "Hello";
+
+            Assert.AreEqual(value, value2);
+        }
+    }
+}
